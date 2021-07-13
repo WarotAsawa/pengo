968d2d6898feb01d2adb4752ddfa21c3dabf67f3
WarotAsawa <warot.asawa@gmail.com>
2021-07-13 12:47:48 +0700

    Clean Button Code on Help

--- pengo/Help.py
+++ pengo/Help.py
@@ -1,4 +1,5 @@
 import math
+from os import stat
 
 from linebot.models.flex_message import BoxComponent, BubbleContainer, ButtonComponent, FlexSendMessage, ImageComponent, TextComponent, CarouselContainer
 
@@ -16,6 +17,11 @@ class Help:
     specToolTip =       "Find specification for each product model\nTip: spec [product] [model]"
     lookupToolTip =     "Lookup match model for selected field value\nTip: lookup [product] [field] [value]"
     sizeToolTip =       "Tip: size [product] [model] [size] [TB/TiB]"
+
+    #Default Flex Button
+    @staticmethod
+    def DefaultButton(label, text):
+        return ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label=label,text=text))
     #Generate help output
     @staticmethod
     def GenerateHelp():
@@ -29,10 +35,10 @@ class Help:
         #Add Header
         headerContents.append(TextComponent(text=specTitle, weight='bold', size='md'))
         contents.append(TextComponent(text=specText, size='xs', wrap=True))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="spec",text='spec')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="spec nimble",text='spec nimble')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="spec rome 7262",text='spec rome 7262')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="spec simplivity 380-XL",text='spec simplivity 380-XL')))
+        contents.append(Help.DefaultButton(label="spec",text='spec'))
+        contents.append(Help.DefaultButton(label="spec nimble",text='spec nimble'))
+        contents.append(Help.DefaultButton(label="spec rome 7262",text='spec rome 7262'))
+        contents.append(Help.DefaultButton(label="spec simplivity 380-XL",text='spec simplivity 380-XL'))
         #Add Bubble's Content
         headerContents.append(BoxComponent(layout='vertical',margin='lg',spacing='sm', contents=contents))
         body = BoxComponent(layout='vertical', contents=headerContents)
@@ -48,10 +54,10 @@ class Help:
         #Add Header
         headerContents.append(TextComponent(text=lookUpTitle, weight='bold', size='md'))
         contents.append(TextComponent(text=lookUpText, size='xs', wrap=True))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="lookup",text='lookup')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="lookup milan",text='lookup milan')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="lookup rome core 64",text='lookup rome core 64')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="lookup cascadelake clock 2.1",text='lookup cascadelake clock 2.l')))
+        contents.append(Help.DefaultButton(label="lookup",text='lookup'))
+        contents.append(Help.DefaultButton(label="lookup milan",text='lookup milan'))
+        contents.append(Help.DefaultButton(label="lookup rome core 64",text='lookup rome core 64'))
+        contents.append(Help.DefaultButton(label="lookup cascadelake clock 2.1",text='lookup cascadelake clock 2.1'))
         #Add Bubble's Content
         headerContents.append(BoxComponent(layout='vertical',margin='lg',spacing='sm', contents=contents))
         body = BoxComponent(layout='vertical', contents=headerContents)
@@ -67,10 +73,10 @@ class Help:
         #Add Header
         headerContents.append(TextComponent(text=sizeTitle, weight='bold', size='md'))
         contents.append(TextComponent(text=sizeText, size='xs', wrap=True))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="size",text='size')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="size primera",text='size primera')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="size nimble",text='size nimble')))
-        contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label="size simplivity",text='size simplivity')))
+        contents.append(Help.DefaultButton(label="size",text='size'))
+        contents.append(Help.DefaultButton(label="size primera",text='size primera'))
+        contents.append(Help.DefaultButton(label="size nimble",text='size nimble'))
+        contents.append(Help.DefaultButton(label="size simplivity",text='size simplivity'))
         #Add Bubble's Content
         headerContents.append(BoxComponent(layout='vertical',margin='lg',spacing='sm', contents=contents))
         body = BoxComponent(layout='vertical', contents=headerContents)
@@ -158,7 +164,7 @@ class Help:
             for j in range(i*maxActionPerColumn,(i*maxActionPerColumn)+maxActionPerColumn):
                 if j >= len(loopList): break
                 else:
-                    contents.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label=loopList[j][0:40], text=textPreFix + loopList[j])))
+                    contents.append(Help.DefaultButton(label=loopList[j][0:40], text=textPreFix + loopList[j]))
              #Add Bubble's Content
             headerContents.append(BoxComponent(layout='vertical',margin='lg',spacing='sm', contents=contents))
             body = BoxComponent(layout='vertical', contents=headerContents)
