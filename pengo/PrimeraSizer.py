759e58db0e6f73be303451b1d424c92eca9cfbae
WarotAsawa <warot.asawa@gmail.com>
2021-07-13 11:49:42 +0700

    Change primera example to Flex

--- pengo/PrimeraSizer.py
+++ pengo/PrimeraSizer.py
@@ -1,7 +1,7 @@
 import math
 import random
 
-from linebot.models.flex_message import BoxComponent, BubbleContainer, FlexSendMessage, TextComponent
+from linebot.models.flex_message import BoxComponent, BubbleContainer, ButtonComponent, FlexSendMessage, ImageComponent, SeparatorComponent, TextComponent
 from AllResponse import AllResponse
 
 from linebot.models import (
@@ -180,30 +180,42 @@ class PrimeraSizer:
         #, quick_reply=quickReply)
 
     @staticmethod
-    def GenerateExampleCarousel(warning):
-        title = "Here is some sizing example"
+    def GenerateExampleCarousel(warning, capacity = 0):
+        title = "Primera sizing example"
+        exampleList = []
         textPreFix = "size primera "
-        exampleList = ["10 TB", "100 TB", "150 TiB", "200.5 TiB", "500 TB", "1000 TiB"]
-        columnList = []
-        #Set Column and Item Limit
-        maxAction = LineConst.maxCarouselColumn * LineConst.maxActionPerColumn
-        #check command's len to prepare return message
-        for i in range(int(math.ceil(len(exampleList)/LineConst.maxActionPerColumn))):
-            if i >= LineConst.maxCarouselColumn: break
-            actions = []
-            for j in range(i*LineConst.maxActionPerColumn,(i*LineConst.maxActionPerColumn)+LineConst.maxActionPerColumn):
-                if j >= maxAction: break
-                if j >= len(exampleList):
-                    actions.append(MessageAction(label=". . .",text=textPreFix))
-                else:
-                    actions.append(MessageAction(label=exampleList[j][0:12],text=textPreFix + exampleList[j]))
-            columnList.append(CarouselColumn(thumbnail_image_url =ImageConst.sizeImage, text='Usage\nsize primera [required usable] [TB/TiB]\nPage '+str(i+1), title=title, actions=actions))
-        carousel_template = CarouselTemplate(columns=columnList)
-        carousel = TemplateSendMessage(
-            alt_text='Sizing Wizard support only on Mobile',
-            template=carousel_template
-        )
-        return [TextSendMessage(text=warning), carousel]
+        #If no capacity generate random units
+        if capacity == 0:
+            for i in range(0,10):
+                unitRand = random.choice([" TB", " TiB"])
+                capaRand = str(random.randint(1,70)*10)
+                exampleList.append(capaRand+unitRand)
+        else:
+            #Is have capacity recommend units
+            exampleList = [str(capacity)+" TB", str(capacity)+" TiB"]
+
+        #Add FLex Content
+        contents = []
+        headerContents = []
+        #Add Header
+        headerContents.append(TextComponent(text=title, weight='bold', size='md',wrap=True))
+        contents.append(TextComponent(text="Tip: size primera [required usable] [TB/TiB]", size='xs', wrap=True))
+        #Add Model Button
+        for i in range(0,math.floor(len(exampleList)/2)):
+            buttonList = []
+            buttonList.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label=exampleList[i*2], text=textPreFix + exampleList[i*2])))
+            buttonList.append(SeparatorComponent(margin='md'))
+            buttonList.append(ButtonComponent(color='#eeeeee',style='secondary',height='sm',action=MessageAction(label=exampleList[i*2+1], text=textPreFix + exampleList[i*2+1])))
+            box = BoxComponent(layout='horizontal',spacing='sm',contents=buttonList)
+            contents.append(box)
+        
+        headerContents.append(BoxComponent(layout='vertical',margin='lg',spacing='sm', contents=contents))
+        body = BoxComponent(layout='vertical', contents=headerContents)
+        hero = ImageComponent(url=ImageConst.sizeImage,background_color=ImageConst.sizeColor,aspect_ratio='20:5',aspect_mode='fit',size='full')
+        bubble = BubbleContainer(direction='ltr',body=body,hero=hero)
+        bubbleMessage = FlexSendMessage(alt_text="Primera Sizing Example", contents=bubble)
+
+        return [TextSendMessage(text=warning), bubbleMessage]
 
     @staticmethod
     def GeneratePrimeraSizer(words):
@@ -233,17 +245,17 @@ class PrimeraSizer:
             except ValueError:
                 return PrimeraSizer.GenerateExampleCarousel("Please input capacity as Decimal") 
             
-            #Check if unit is tb or tib
-            unit = words[3].lower()
-            unitCheck = ["tb","tib"]
-            #unitCheck = ["tb","tib", "gb", "gib", "pb", "pib"]
-            if unit not in unitCheck:
-                return PrimeraSizer.GenerateExampleCarousel("Please input unit as TB or TiB") 
             #Capacity Check
             multiplier = Converter.TBToUnitMultipler(unit)
             convertedRequired = required * multiplier * 100 / utilization
             if convertedRequired <= 0 or convertedRequired > 2711.33:  
                 return PrimeraSizer.GenerateExampleCarousel("Please input capacity between 0 and 2711") 
+            #Check if unit is tb or tib
+            unit = words[3].lower()
+            unitCheck = ["tb","tib"]
+            #unitCheck = ["tb","tib", "gb", "gib", "pb", "pib"]
+            if unit not in unitCheck:
+                return PrimeraSizer.GenerateExampleCarousel("Please input unit as TB or TiB", capacity=required) 
             #Get Sizing
             return PrimeraSizer.GeneratePrimeraSizeAnswers(unit = unit, required = required, utilization = utilization)
 
